95197ae37879af709b919eb8824b7bf172d541ae
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-08-29 13:59:51 +0000

    chore(release): v7.0.0

--- http/datahttp/DataHttpRetryOutputReference.go
+++ http/datahttp/DataHttpRetryOutputReference.go
@@ -1,10 +1,13 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package datahttp
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-http-go/http/v6/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-http-go/http/v7/jsii"
 
-	"github.com/cdktf/cdktf-provider-http-go/http/v6/datahttp/internal"
+	"github.com/cdktf/cdktf-provider-http-go/http/v7/datahttp/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- http/go.mod
+++ http/go.mod
@@ -1,9 +1,9 @@
-module github.com/cdktf/cdktf-provider-http-go/http/v6
+module github.com/cdktf/cdktf-provider-http-go/http/v7
 
 go 1.18
 
 require (
-	github.com/aws/jsii-runtime-go v1.84.0
-	github.com/hashicorp/terraform-cdk-go/cdktf v0.17.0
-	github.com/aws/constructs-go/constructs/v10 v10.2.56
+	github.com/aws/jsii-runtime-go v1.87.0
+	github.com/hashicorp/terraform-cdk-go/cdktf v0.18.0
+	github.com/aws/constructs-go/constructs/v10 v10.2.69
 )
