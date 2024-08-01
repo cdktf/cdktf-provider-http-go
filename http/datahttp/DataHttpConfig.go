08b91af098521d19a9c53fce56eb6def6b09b809
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-08-01 03:40:01 +0000

    chore(release): v9.0.3

--- http/datahttp/DataHttpConfig.go
+++ http/datahttp/DataHttpConfig.go
@@ -24,37 +24,37 @@ type DataHttpConfig struct {
 	Provisioners *[]interface{} `field:"optional" json:"provisioners" yaml:"provisioners"`
 	// The URL for the request. Supported schemes are `http` and `https`.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#url DataHttp#url}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#url DataHttp#url}
 	Url *string `field:"required" json:"url" yaml:"url"`
 	// Certificate data of the Certificate Authority (CA) in [PEM (RFC 1421)](https://datatracker.ietf.org/doc/html/rfc1421) format.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#ca_cert_pem DataHttp#ca_cert_pem}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#ca_cert_pem DataHttp#ca_cert_pem}
 	CaCertPem *string `field:"optional" json:"caCertPem" yaml:"caCertPem"`
 	// Disables verification of the server's certificate chain and hostname. Defaults to `false`.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#insecure DataHttp#insecure}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#insecure DataHttp#insecure}
 	Insecure interface{} `field:"optional" json:"insecure" yaml:"insecure"`
 	// The HTTP Method for the request.
 	//
 	// Allowed methods are a subset of methods defined in [RFC7231](https://datatracker.ietf.org/doc/html/rfc7231#section-4.3) namely, `GET`, `HEAD`, and `POST`. `POST` support is only intended for read-only URLs, such as submitting a search.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#method DataHttp#method}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#method DataHttp#method}
 	Method *string `field:"optional" json:"method" yaml:"method"`
 	// The request body as a string.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#request_body DataHttp#request_body}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#request_body DataHttp#request_body}
 	RequestBody *string `field:"optional" json:"requestBody" yaml:"requestBody"`
 	// A map of request header field names and values.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#request_headers DataHttp#request_headers}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#request_headers DataHttp#request_headers}
 	RequestHeaders *map[string]*string `field:"optional" json:"requestHeaders" yaml:"requestHeaders"`
 	// The request timeout in milliseconds.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#request_timeout_ms DataHttp#request_timeout_ms}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#request_timeout_ms DataHttp#request_timeout_ms}
 	RequestTimeoutMs *float64 `field:"optional" json:"requestTimeoutMs" yaml:"requestTimeoutMs"`
 	// retry block.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#retry DataHttp#retry}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#retry DataHttp#retry}
 	Retry *DataHttpRetry `field:"optional" json:"retry" yaml:"retry"`
 }
 
--- http/datahttp/DataHttpRetry.go
+++ http/datahttp/DataHttpRetry.go
@@ -9,15 +9,15 @@ type DataHttpRetry struct {
 	//
 	// For example, if 2 is specified, the request will be tried a maximum of 3 times.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#attempts DataHttp#attempts}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#attempts DataHttp#attempts}
 	Attempts *float64 `field:"optional" json:"attempts" yaml:"attempts"`
 	// The maximum delay between retry requests in milliseconds.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#max_delay_ms DataHttp#max_delay_ms}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#max_delay_ms DataHttp#max_delay_ms}
 	MaxDelayMs *float64 `field:"optional" json:"maxDelayMs" yaml:"maxDelayMs"`
 	// The minimum delay between retry requests in milliseconds.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.3/docs/data-sources/http#min_delay_ms DataHttp#min_delay_ms}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.4/docs/data-sources/http#min_delay_ms DataHttp#min_delay_ms}
 	MinDelayMs *float64 `field:"optional" json:"minDelayMs" yaml:"minDelayMs"`
 }
 
