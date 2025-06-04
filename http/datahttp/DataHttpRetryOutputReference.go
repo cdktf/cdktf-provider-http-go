3c344ca3b80ccf31a2470439c9f63bcf316324cd
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-06-04 18:56:26 +0000

    chore(release): v10.0.0

--- http/datahttp/DataHttpRetryOutputReference.go
+++ http/datahttp/DataHttpRetryOutputReference.go
@@ -5,9 +5,9 @@ package datahttp
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-http-go/http/v9/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-http-go/http/v10/jsii"
 
-	"github.com/cdktf/cdktf-provider-http-go/http/v9/datahttp/internal"
+	"github.com/cdktf/cdktf-provider-http-go/http/v10/datahttp/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
