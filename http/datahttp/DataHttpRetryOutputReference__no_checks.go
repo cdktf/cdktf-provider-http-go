9471753f566deec24041940faff093fef81a5b29
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-12-10 22:14:31 +0000

    chore(release): v10.0.1

--- http/datahttp/DataHttpRetryOutputReference__no_checks.go
+++ http/datahttp/DataHttpRetryOutputReference__no_checks.go
@@ -43,11 +43,11 @@ func (d *jsiiProxy_DataHttpRetryOutputReference) validateGetStringMapAttributePa
 	return nil
 }
 
-func (d *jsiiProxy_DataHttpRetryOutputReference) validateInterpolationForAttributeParameters(property *string) error {
+func (d *jsiiProxy_DataHttpRetryOutputReference) validateInterpolationForAttributeParameters(terraformAttribute *string) error {
 	return nil
 }
 
-func (d *jsiiProxy_DataHttpRetryOutputReference) validateResolveParameters(_context cdktf.IResolveContext) error {
+func (d *jsiiProxy_DataHttpRetryOutputReference) validateResolveParameters(context cdktf.IResolveContext) error {
 	return nil
 }
 
